9189fc95bb712d3577d4dfd27d8ba7a470cf3672
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2024-05-30 19:54:02 -0500

    Update user_controller.dart

--- lib/ui/controller/user_controller.dart
+++ lib/ui/controller/user_controller.dart
@@ -1,8 +1,8 @@
 import 'package:get/get.dart';
 import 'package:loggy/loggy.dart';
-
 import '../../domain/models/user.dart';
 import '../../domain/use_case/user_usecase.dart';
+import 'authentication_controller.dart';
 
 class UserController extends GetxController {
   final RxList<User> _users = <User>[].obs;
@@ -18,6 +18,10 @@ class UserController extends GetxController {
 
   getUers() async {
     logInfo("Getting users");
+    AuthenticationController authController = Get.find();
+    if (authController.isLogged == false) {
+      return;
+    }
     _users.value = await userUseCase.getUsers();
   }
 
